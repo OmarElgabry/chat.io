946959eaa47ed7f59b1db75d0802a217a82d58e1
Omar El Gabry <omar.elgabry.93@gmail.com>
2016-12-29 00:17:22 +0200

    Fix & Avoid expired Facebook CDN URLs
    Updated all existing Facebook expired URLs of profile pictures by running a command on database from localhost.
    i.e. if(user.picture.indexOf("fbcdn") > -1) { ... update user's profile picture value and save() ... }

--- app/models/user.js
+++ app/models/user.js
@@ -26,12 +26,21 @@ var findOrCreate = function(data, callback){
 		if(err) { return callback(err); }
 		if(user){
 			return callback(err, user);
-		}else{
-			create({
+		} else {
+			var userData = {
 				username: data.displayName,
 				socialId: data.id,
 				picture: data.photos[0].value || null
-			}, function(err, newUser){
+			};
+
+			// To avoid expired Facebook CDN URLs
+			// Request user's profile picture using user id 
+			// @see http://stackoverflow.com/a/34593933/6649553
+			if(data.provider == "facebook" && userData.picture){
+				userData.picture = "http://graph.facebook.com/" + data.id + "/picture?type=large";
+			}
+
+			create(userData, function(err, newUser){
 				callback(err, newUser);
 			});
 		}
